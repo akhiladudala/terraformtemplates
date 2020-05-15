f03cc2286a73e1f0f7f7f319203cd9b8d97bce49
akhiladudala <65311249+akhiladudala@users.noreply.github.com>
2020-05-15 17:18:54 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,9 +4,9 @@ variable "iaas_classic_api_key" {}
 variable "region" {}
 
 provider "ibm" {
-ibmcloud_api_key = var.ibmcloud_api_key
+ibmcloud_api_key = "xkYslsB-A37D1OrrVKRgMseyTqGsFthqklYhcdCdAQsl"
 generation = 1
-region = var.region
-iaas_classic_username = var.iaas_classic_username
-iaas_classic_api_key  = var.iaas_classic_api_key
+region = "us-south"
+iaas_classic_username = "SL2084148"
+iaas_classic_api_key  = "b437c6a8f90e4889c736673a3100a4a63ffbb9be58cc7d54e785f9dd95620c11"
 }
